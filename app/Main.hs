fa0f2f360572221cc518cd248b96ae92cabb4523
Daisuke Shimamoto <diskshima@gmail.com>
2018-04-08 10:30:27 +0800

    Incoporate GetOpt

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main where
 
-import Lib
+import           Lib
+import           System.Console.GetOpt (ArgDescr (..), ArgOrder (RequireOrder),
+                                        OptDescr (..), getOpt, usageInfo)
+import           System.Environment    (getArgs)
+
+data Flag =
+  Verbose |
+  Version |
+  Issues
+
+options :: [OptDescr Flag]
+options = [
+  Option ['v']["verbose"] (NoArg Verbose) "Verbose output"
+          ]
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  case getOpt RequireOrder options args of
+    (o, n, [])   -> print n
+    (_, _, errs) -> ioError (userError (concat errs ++ usageInfo header options))
+  where header = "Usage: gwcli subcommand"
