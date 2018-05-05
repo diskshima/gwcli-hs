a0d0f4110d8b7c96c596e0d7863ca73ec76c32c4
Daisuke Shimamoto <diskshima@gmail.com>
2018-05-05 23:50:40 +0900

    Use lower camel case for field names

--- src/GitHub.hs
+++ src/GitHub.hs
@@ -3,23 +3,30 @@
 module GitHub where
 
 import           Control.Lens.Operators  ((.~), (^.))
-import           Data.Aeson              (FromJSON, decode)
+import           Data.Aeson              (FromJSON (parseJSON), decode,
+                                          defaultOptions, fieldLabelModifier,
+                                          genericParseJSON)
+import           Data.Aeson.Casing       (snakeCase)
 import qualified Data.ByteString         as BS
 import qualified Data.ByteString.Lazy    as BL
 import qualified Data.ByteString.UTF8    as U8
 import           Data.Function           ((&))
+import           Data.List               (intercalate)
+import           Data.Maybe              (fromMaybe)
 import           Data.String.Conversions (convertString)
 import           GHC.Generics
 import           Network.Wreq            (Options, Response, defaults, getWith,
                                           header, responseBody)
+import           Text.Printf             (printf)
 
 data Issue = Issue {
-  id       :: Integer,
-  html_url :: String,
-  title    :: String
+  id      :: Integer,
+  htmlUrl :: String,
+  title   :: String
 } deriving (Show, Generic)
 
-instance FromJSON Issue
+instance FromJSON Issue where
+  parseJSON = genericParseJSON defaultOptions { fieldLabelModifier = snakeCase }
 
 gitHubBaseUrl :: String
 gitHubBaseUrl = "https://api.github.com"
@@ -31,8 +38,12 @@ getGitHub :: Maybe String -> String -> IO (Response BL.ByteString)
 getGitHub token path = getWith opt (gitHubBaseUrl ++ path)
   where opt = maybe defaults gitHubHeader token
 
+formatIssue :: Issue -> String
+formatIssue i = printf "%s: %s" (title i) (htmlUrl i)
+
 getIssues :: [String] -> Maybe String -> IO ()
 getIssues sscmds token = do
   resp <- getGitHub token "/repos/organization/repo/issues"
-  let result = decode (resp ^. responseBody) :: Maybe [Issue] in
-    print result
+  let result = decode (resp ^. responseBody) :: Maybe [Issue]
+      issues = fromMaybe [] result in
+    putStrLn $ intercalate "\n" (fmap formatIssue issues)
