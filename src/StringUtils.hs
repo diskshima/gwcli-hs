bd2ffda783807fcc0de2a6792840230ec410721e
Daisuke Shimamoto <diskshima@gmail.com>
2019-07-20 16:58:45 +0900

    Add replace method

--- src/StringUtils.hs
+++ src/StringUtils.hs
@@ -1,9 +1,25 @@
 module StringUtils
   (
     orBlank
+  , replace
   ) where
 
 import           Data.Maybe (fromMaybe)
 
 orBlank :: Maybe String -> String
 orBlank = fromMaybe ""
+
+-- Token from
+--   https://stackoverflow.com/a/14880881
+replace :: Eq a => [a] -> [a] -> [a] -> [a]
+replace needle replacement haystack
+  = case begins haystack needle of
+      Just remains -> replacement ++ remains
+      Nothing      -> case haystack of
+                        []     -> []
+                        x : xs -> x : replace needle replacement xs
+
+begins :: Eq a => [a] -> [a] -> Maybe [a]
+begins haystack []       = Just haystack
+begins (x : xs) (y : ys) | x == y = begins xs ys
+begins _        _        = Nothing
