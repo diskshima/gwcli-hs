edcaad50addd4476715b5d62314bf1da368944ed
Daisuke Shimamoto <diskshima@gmail.com>
2020-07-19 15:55:20 +0900

    Options for issue and pull request creation (#37)
    #33

--- app/Main.hs
+++ app/Main.hs
@@ -7,8 +7,7 @@ import           CredentialUtils       (Credentials (..), credFilePath,
                                         readCredential, writeCredential)
 import           Data.List             (isInfixOf, isPrefixOf)
 import           GitUtils              (getCurrentBranch, getRemoteUrl)
-import           ListUtils             (formatEachAndJoin, nthOrDefault,
-                                        nthOrNothing)
+import           ListUtils             (formatEachAndJoin)
 import           Remote                (authenticate, createIssue,
                                         createPullRequest, getIssue,
                                         getPullRequest, listIssues,
@@ -30,75 +29,117 @@ options =
   , Option ['h']["help"] (NoArg Help) "Help"
   ]
 
-newtype IssueOptions = IssueOptions { iOptAll :: Bool }
-issueOptions :: [OptDescr (IssueOptions -> IssueOptions)]
-issueOptions =
+newtype IssueListOptions = IssueListOptions { iOptAll :: Bool }
+
+defaultIssueListOptions :: IssueListOptions
+defaultIssueListOptions = IssueListOptions { iOptAll = False }
+
+issueListOptions :: [OptDescr (IssueListOptions -> IssueListOptions)]
+issueListOptions =
   [ Option ['a']["all"]
       (NoArg (\opts -> opts { iOptAll = True }))
        "show all issues"
   ]
 
-defaultIssueOptions :: IssueOptions
-defaultIssueOptions = IssueOptions { iOptAll = False }
+data IssueCreateOptions =
+  IssueCreateOptions { iscoTitle :: String, iscoBody :: String }
 
-newtype PullRequestOptions = PullRequestOptions { prOptAll :: Bool }
-pullRequestOptions :: [OptDescr (PullRequestOptions -> PullRequestOptions)]
-pullRequestOptions =
+defaultIssueCreateOptions :: IssueCreateOptions
+defaultIssueCreateOptions = IssueCreateOptions { iscoTitle = "", iscoBody = "" }
+
+issueCreateOptions :: [OptDescr (IssueCreateOptions -> IssueCreateOptions)]
+issueCreateOptions =
+  [ Option ['t'] ["title"]
+      (ReqArg (\title opts -> opts { iscoTitle = title }) "TITLE")
+      "Issue title"
+  , Option ['m'] ["message"]
+      (ReqArg (\msg opts -> opts { iscoBody = msg }) "BODY")
+      "Issue message (body)"
+  ]
+
+newtype PullRequestListOptions = PullRequestListOptions { prOptAll :: Bool }
+
+defaultPullRequestListOptions :: PullRequestListOptions
+defaultPullRequestListOptions = PullRequestListOptions { prOptAll = False }
+
+pullRequestListOptions :: [OptDescr (PullRequestListOptions -> PullRequestListOptions)]
+pullRequestListOptions =
   [ Option ['a']["all"]
       (NoArg (\opts -> opts { prOptAll = True }))
        "show all pull requests"
   ]
 
-defaultPullRequestOptions :: PullRequestOptions
-defaultPullRequestOptions = PullRequestOptions { prOptAll = False }
+data PullRequestCreateOptions =
+  PullRequestCreateOptions { prcoBase :: String , prcoTitle :: String, prcoBody :: String }
+
+defaultPullRequestCreateOptions :: PullRequestCreateOptions
+defaultPullRequestCreateOptions =
+  PullRequestCreateOptions { prcoBase = "master" , prcoTitle = "", prcoBody = "" }
+
+pullRequestCreateOptions :: [OptDescr (PullRequestCreateOptions -> PullRequestCreateOptions)]
+pullRequestCreateOptions =
+  [ Option ['t'] ["title"]
+      (ReqArg (\title opts -> opts { prcoTitle = title }) "TITLE")
+      "Pull request title"
+  , Option ['b'] ["base"]
+      (ReqArg (\base opts -> opts { prcoBase = base }) "BRANCH")
+      "Base (destination) branch"
+  , Option ['m'] ["message"]
+      (ReqArg (\msg opts -> opts { prcoBody = msg }) "BODY")
+      "Pull request message (body)"
+  ]
 
 printError :: String -> IO ()
 printError = ioError . userError
 
-paramToIssue :: [String] -> I.Issue
-paramToIssue params = I.Issue Nothing title body Nothing
-  where title = head params
-        body = nthOrNothing params 1
+paramsToIssue :: IssueCreateOptions -> I.Issue
+paramsToIssue params = I.Issue Nothing title (Just body) Nothing
+  where IssueCreateOptions { iscoTitle = title, iscoBody = body } = params
 
-paramsToPullRequest :: [String] -> IO PR.PullRequest
-paramsToPullRequest params = do
+paramsToPullRequest :: PullRequestCreateOptions -> IO PR.PullRequest
+paramsToPullRequest opts = do
   maybeBranch <- getCurrentBranch
   case maybeBranch of
-    Just src -> return $ PR.PullRequest Nothing title src dest body Nothing
+    Just src -> return $ PR.PullRequest Nothing title src base (Just body) Nothing
     Nothing  -> error "Failed to retrieve source branch."
-  where title = head params
-        dest = nthOrDefault params "master" 1
-        body = nthOrNothing params 2
+  where
+    PullRequestCreateOptions { prcoTitle = title, prcoBase = base, prcoBody = body } = opts
 
 handleIssue :: Remote -> [String] -> IO ()
 handleIssue remote params
   | ssc `isPrefixOf` "show" = getIssue remote (head rest) >>= (putStrLn . I.formatIssue)
   | ssc `isPrefixOf` "list" = do
+    let (parsed, _, _) = getOpt RequireOrder issueListOptions rest
+        IssueListOptions { iOptAll = showAll } = foldl (flip id) defaultIssueListOptions parsed
     issues <- listIssues remote showAll
     putStrLn $ formatEachAndJoin issues I.formatIssue
-  | ssc `isPrefixOf` "create" = createIssue remote (paramToIssue rest)
-                  >>= (putStrLn . I.formatIssue)
+  | ssc `isPrefixOf` "create" = do
+      let (parsed, _, _) = getOpt RequireOrder issueCreateOptions rest
+          cParams = foldl (flip id) defaultIssueCreateOptions parsed
+      response <- createIssue remote (paramsToIssue cParams)
+      putStrLn $ I.formatIssue response
   | otherwise = printError $ "Subcommand " ++ ssc ++ " not supported"
     where ssc = head params
           rest = tail params
-          (parsed, _, _) = getOpt RequireOrder issueOptions rest
-          IssueOptions { iOptAll = showAll } = foldl (flip id) defaultIssueOptions parsed
 
 handlePullRequest :: Remote -> [String] -> IO ()
 handlePullRequest remote params
   | ssc `isPrefixOf` "show" = getPullRequest remote (head rest) >>= (putStrLn . PR.formatPullRequest)
   | ssc `isPrefixOf` "list" = do
+      let (parsed, _, _) = getOpt RequireOrder pullRequestListOptions rest
+          PullRequestListOptions { prOptAll = showAll } =
+            foldl (flip id) defaultPullRequestListOptions parsed
       prs <- listPullRequests remote showAll
       putStrLn $ formatEachAndJoin prs PR.formatPullRequest
   | ssc `isPrefixOf` "create" = do
-      pr <- paramsToPullRequest rest
+      let (parsed, _, _) = getOpt RequireOrder pullRequestCreateOptions rest
+          cParams = foldl (flip id) defaultPullRequestCreateOptions parsed
+      pr <- paramsToPullRequest cParams
       response <- createPullRequest remote pr
       putStrLn $ PR.formatPullRequest response
   | otherwise = printError $ "Command " ++ ssc ++ " not supported"
     where ssc = head params
           rest = tail params
-          (parsed, _, _) = getOpt RequireOrder pullRequestOptions rest
-          PullRequestOptions { prOptAll = showAll } = foldl (flip id) defaultPullRequestOptions parsed
 
 handleAuth :: Remote -> Credentials -> FilePath -> IO ()
 handleAuth remote creds credFP = do
